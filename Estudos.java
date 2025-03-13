7f7018d8282123606a7053436b8eaa231724edd1
VitorHugoLecryman <vhsouzatavares@gmail.com>
2025-03-13 09:25:13 -0300

    Projetinho Java

--- Estudos.java
+++
@@ -1,7 +0,0 @@
-public class Estudos {
-    public static void main(String[] args) {
-        String notes = "Hoje na aula não aprendemos nada sobre java, porém tivemos aula de cloud.\n\nNeste exato momento estou estudando através dos cursos da alura.";
-
-        System.out.println(notes);
-    }
-}
