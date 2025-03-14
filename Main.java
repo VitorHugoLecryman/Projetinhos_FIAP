052a9bd8bd7d898b392a196cb4c31b5f733a89dd
VitorHugoLecryman <vhsouzatavares@gmail.com>
2025-03-14 11:12:44 -0300

    Teste

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         String mensagem1 = "A user is connected.";
-        String mensagem2 = "Welcome Vvvv.";
+        String mensagem2 = "Welcome Vvvvvv.";
 
         System.out.println(mensagem1);
         System.out.println(mensagem2);
